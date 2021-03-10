5c68013c51f4a552a97ac695da98e6750c032532
vladislava8sv <57909332+vladislava8sv@users.noreply.github.com>
2021-03-10 16:15:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function toReadable (number) {
-  
+
+    let SingleNumber = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let DoubleNumber = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let DozensNumber = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    //трехзначные числа-максимум
+    //разложим число на разряды   126
+    let numberValue = number;
+    let firstRank = numberValue % 10;         
+    numberValue = (numberValue - firstRank ) / 10       
+    let secondRank  = numberValue % 10;       
+    numberValue = (numberValue - secondRank ) / 10          
+    let thirdRank  = numberValue % 10;       
+    numberValue = (numberValue - thirdRank ) / 10
+
+    let FirstTest;
+    let SecondTest;
+    let ThirdTest; 
+    
+    if (thirdRank != 0) {
+        ThirdTest = `${SingleNumber[thirdRank]} hundred`;
+    } else {
+        ThirdTest = '';
+    }
+
+    if (secondRank == 1) {
+        SecondTest = DoubleNumber[firstRank];//тк 10-19 пишется в одно слово, мы сравниваем по единицам(1 ранг)
+    }
+    else if (secondRank == 0){
+        SecondTest = '';
+    } 
+    else if (secondRank > 1 ) {
+        SecondTest = DozensNumber[secondRank-2];//тк 10 храниться с 10-19, индекс массива начинается с 0 
+    }
+
+    if ((firstRank == 0 && (thirdRank != 0 || secondRank != 0) || secondRank == 1)){
+        FirstTest = '';
+    }
+    else  {
+        FirstTest = SingleNumber[firstRank];
+    } 
+
+    let result = [ThirdTest, SecondTest, FirstTest];
+    return result.filter(num => num !== '').join(' ');//если число непустое, то соединяем с пробелами
 }
